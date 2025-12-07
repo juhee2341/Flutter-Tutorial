6c9fdd92b3dee2c87e0aa88f9ec6dd357b2968bf
juhee2341 <210421zz@gmail.com>
2025-12-07 21:48:16 +0900

    layout fix

--- 04_assigment_1/basic_layout/lib/feature/otp_verification/otp_verification_screen.dart
+++ 04_assigment_1/basic_layout/lib/feature/otp_verification/otp_verification_screen.dart
@@ -8,35 +8,72 @@ class OtpVerificationScreen extends StatefulWidget {
 }
 
 class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
+  TextEditingController mobileNumber = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Expanded(
-            child: Center(
-              child: Column(
-                children: [
-                  Image.network(
-                    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
-                    fit: BoxFit.cover,
-                    width: 300,
-                    height: 300,
-                    frameBuilder:
-                        (context, child, frame, wasSynchronouslyLoaded) {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(4),
-                            child: child,
-                          );
-                        },
-                  ),
-                  const Text(
-                    'OTP Verification',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 40),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.network(
+                      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+                      fit: BoxFit.cover,
+                      width: 300,
+                      height: 300,
+                      frameBuilder:
+                          (context, child, frame, wasSynchronouslyLoaded) {
+                            return ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: child,
+                            );
+                          },
                     ),
-                  ),
-                ],
+                    const Text(
+                      'OTP Verification',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const Text(
+                      'We will send you a one-time password on this mobile number',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    const Text(
+                      'Enter Mobile Number',
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                    TextField(
+                      controller: mobileNumber,
+                      decoration: const InputDecoration(
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.all(12), // 위치 예쁘게 정리
+                          child: Text(
+                            "+82",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        prefixIconConstraints: BoxConstraints(),
+                      ),
+                      keyboardType: TextInputType.phone,
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blueAccent,
+                        minimumSize: const Size(300, 50),
+                      ),
+                      child: const Text("Get OTP"),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
